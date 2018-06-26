65aa835aeee2b6d4c531deb158054ae736d64879
Hyesun Suk <Hyesun.Suk@ppdi.com>
2018-06-26 09:56:12 -0400

    Building from Json data

--- js/main.js
+++ js/main.js
@@ -4,5 +4,32 @@
 *    2.8 - Activity: Your first visualization!
 */
 d3.json("data/buildings.json").then(function(data){
-  console.log(data);
-})
+  data.forEach(function(d){
+    d.height = +d.height;
+  });
+
+var svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+var rects = svg.selectAll("rect")
+  .data(data);
+
+  rects.enter()
+  .append("rect")
+  .attr("y", (d, i) => {
+    return (i * 50) + 25;
+  })
+  .attr("width", function(d, i){
+    return 25;
+  })
+  .attr("height", function(d){
+    return d.height * 2;
+  })
+  .attr("fill", function(d){
+    if(d.name === "Burj Khalifa"){
+      return "blue";
+    }else {
+      return "red"
+    }
+  });
