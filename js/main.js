9b735e40bf80b2a677806b75fcdaed3d869d6e91
Hyesun Suk <Hyesun.Suk@ppdi.com>
2018-06-26 09:44:36 -0400

    First D3 Excersize

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    2.8 - Activity: Your first visualization!
+*/
+d3.json("data/buildings.json").then(function(data){
+  console.log(data);
+})
