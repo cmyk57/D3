e14230e3b92d8b61f14e29083af3ed12e73c3f2d
Hyesun Suk <Hyesun.Suk@ppdi.com>
2018-06-26 09:59:53 -0400

    Jason data testing

--- js/main.js
+++ js/main.js
@@ -3,33 +3,22 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
-d3.json("data/buildings.json").then(function(data){
-  data.forEach(function(d){
-    d.height = +d.height;
-  });
+var data = [25, 20, 10, 12, 15];
 
 var svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400);
 
-var rects = svg.selectAll("rect")
+var circles = svg.selectAll("circle")
   .data(data);
 
-  rects.enter()
-  .append("rect")
-  .attr("y", (d, i) => {
+  circles.enter()
+  .append("circle")
+  .attr("cx", (d, i) => {
     return (i * 50) + 25;
   })
-  .attr("width", function(d, i){
-    return 25;
+  .attr("cy", 25)
+  .attr("r", function(d){
+    return d;
   })
-  .attr("height", function(d){
-    return d.height * 2;
-  })
-  .attr("fill", function(d){
-    if(d.name === "Burj Khalifa"){
-      return "blue";
-    }else {
-      return "red"
-    }
-  });
+  .attr("fill", "red");
