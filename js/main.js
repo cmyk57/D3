479ef94c013da6c3c3f8ea539ffa234d792bfc9b
Hyesun Suk <Hyesun.Suk@ppdi.com>
2018-06-26 10:03:49 -0400

    JSON testing

--- js/main.js
+++ js/main.js
@@ -9,16 +9,21 @@ var svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400);
 
-var circles = svg.selectAll("circle")
+var rects = svg.selectAll("rect")
   .data(data);
 
-  circles.enter()
-  .append("circle")
-  .attr("cx", (d, i) => {
+  rects.enter()
+  .append("rect")
+  .attr("y", (d, i) => {
     return (i * 50) + 25;
   })
-  .attr("cy", 25)
-  .attr("r", function(d){
-    return d;
+  .attr("width", function(d, i){
+    return 25;
   })
-  .attr("fill", "red");
+  .attr("height", function(d){
+    return 25;
+  })
+  .attr("fill", function(d){
+      return "red"
+    }
+  });
